20517fc2990481ae47e477a5dfa314c6a9104cfe
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-04 11:53:35 -0700

    Time: 207 ms (31.21%) | Memory: 77.8 MB (48.11%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -18,18 +18,22 @@ class Solution:
         queue.appendleft(1)
         connected = []
 
+        # ans = basically the lowest path out of all the edges connected to node 1
+        ans = 10000000 # big number it will never hit
+
         while queue:
             curr = queue.popleft()
             for child, dist in adj[curr]:
+                # sneaky sneaky line:
+                # you're gonna traverse every connected node once bc of the if statement
+                # but you ALSO traverse already visited children and do nothing
+                # so then putting this line here allows u to get all the adjacent nodes to the current node
+                # essentially getting every edge distance
+                # smth like that
+                ans = min(ans, dist)
                 if child not in visited:
                     visited.add(child)
                     queue.appendleft(child)
                     connected.append(child)
-
-        # ans = basically the lowest path out of all the edges connected to node 1
-        ans = 10000000 # big number it will never hit
-        for connected_node in connected:
-            for child, dist in adj[connected_node]:
-                ans = min(ans, dist)
 
         return ans
