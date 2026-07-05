a0d1809074e1ef43282d94d9230784f057c22ad6
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-05 16:17:22 -0700

    Time: 123 ms (5.35%) | Memory: 21.4 MB (10.51%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,46 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        # Find the first nonnegative number
+        start_pos = None
+        for i, num in enumerate(nums):
+            if num >= 0:
+                start_pos = i
+                break
+
+        print(start_pos)
+
+        if start_pos is None:
+            # they're all negative
+            return [num ** 2 for num in nums[::-1]]
+        elif start_pos == 0:
+            # they're all positive
+            return [num ** 2 for num in nums]
+
+        # mix of negative + positive
+        i = start_pos # + pointer
+        j = start_pos - 1 # - pointer
+        ans = []
+
+        while i < len(nums) and j >= 0:
+            print("compare", nums[i], -nums[j])
+            if nums[i] <= -nums[j]:
+                # i wins
+                ans.append(nums[i] ** 2)
+                print(nums[i], "wins")
+                i += 1
+            else:
+                # j wins
+                ans.append(nums[j] ** 2)
+                print(-nums[j], "wins")
+                j -= 1
+
+        while i < len(nums):
+            ans.append(nums[i] ** 2)
+            i += 1
+
+        while j >= 0:
+            # j wins
+            ans.append(nums[j] ** 2)
+            j -= 1
+        
+        return ans
