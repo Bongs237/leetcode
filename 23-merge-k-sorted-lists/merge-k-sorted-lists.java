50cf8e10b167a7216c61d9e3b49407c42f900d18
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-06 15:10:21 -0700

    Time: 162 ms (5.03%) | Memory: 47.1 MB (5.23%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode newPtr;
+
+    // just link the actual node
+    public void add(ListNode node) {
+        newPtr.next = node;
+        newPtr = newPtr.next;
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            System.out.println("HIIIII");
+            System.out.println(list2);
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        // Make dummy node
+        ListNode dummy = new ListNode(0);
+
+        ListNode pt1 = list1;
+        ListNode pt2 = list2;
+
+        newPtr = dummy;
+
+        // Basically it's a race
+        // If first one smaller, move first pointer
+        // If second one smaller, move second one type thing
+        while (pt1 != null && pt2 != null) {
+            if (pt2.val < pt1.val) {
+                add(pt2);
+                pt2 = pt2.next;
+            } else {
+                add(pt1);
+                pt1 = pt1.next;
+            }
+        }
+
+        // until one of them is null
+        while (pt1 != null) {
+            add(pt1);
+            pt1 = pt1.next;
+        }
+        while (pt2 != null) {
+            add(pt2);
+            pt2 = pt2.next;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        if (lists.length == 1) return lists[0];
+
+        ListNode theList = lists[0];
+        System.out.println(theList == null);
+        for (int i = 1; i < lists.length; i++) {
+            theList = mergeTwoLists(theList, lists[i]);
+        }
+        return theList;
+    }
+}
