d17d8be48d678ac49eb290b92fbf55bd687a5982
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-08 14:27:33 -0700

    Time: 3 ms (39.73%) | Memory: 59.5 MB (45.21%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.ts
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.ts
@@ -12,37 +12,4 @@
  * }
  */
 
-function sortedArrayToBST(nums: number[]): TreeNode | null {
-    // Ugly iterative method
-    const stack = [];
-    let root;
-
-    // Structure: [left, right, parent node, child type "left" or "right"]
-    stack.push([0, nums.length - 1, null, null]);
-
-    while (stack.length != 0) {
-        const [left, right, parent, childType] = stack.pop();
-        // Add to bst
-        const mid = left + Math.floor((right - left) / 2);
-
-        const newNode = new TreeNode(nums[mid]);
-        if (parent == null) {
-            // Set the root
-            root = newNode;
-        } else if (childType == "left") {
-            parent.left = newNode;
-        } else { // right
-            parent.right = newNode;
-        }
-
-        // Compute range of left + push
-        const leftRange = (mid - 1) - left + 1;
-        if (leftRange >= 1) stack.push([left, mid - 1, newNode, "left"]);
-
-        // Compute range of right + push
-        const rightRange = right - (mid + 1) + 1
-        if (rightRange >= 1) stack.push([mid + 1, right, newNode, "right"]);
-    }
-
-    return root;
-};
+const sortedArrayToBST=r=>{if(!r.length)return null;let e=Math.floor(r.length/2),t=new TreeNode(r[e]);return t.left=sortedArrayToBST(r.slice(0,e)),t.right=sortedArrayToBST(r.slice(e+1)),t};
