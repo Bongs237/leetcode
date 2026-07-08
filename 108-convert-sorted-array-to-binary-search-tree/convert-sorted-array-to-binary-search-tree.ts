82cb19dbb2504a1ce7c094ae06401b645a0dcb3b
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-08 14:29:00 -0700

    Time: 2 ms (71.51%) | Memory: 60 MB (11.23%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.ts
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.ts
@@ -12,20 +12,6 @@
  * }
  */
 
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
 function sortedArrayToBST(nums: number[]): TreeNode | null {
     // Strat: Keep picking the mid and then add the mid of the left and right sublists
     function addMid(left: number, right: number) {
@@ -45,39 +31,7 @@ function sortedArrayToBST(nums: number[]): TreeNode | null {
         return node;
     }
 
-    const stack = [];
-    let root;
-
-    // [left, right, parent node, child type "left" or "right"]
-    stack.push([0, nums.length - 1, null, null]);
-
-    while (stack.length != 0) {
-        const [left, right, parent, childType] = stack.pop();
-        // Add to bst
-        const mid = left + Math.floor((right - left) / 2);
-
-        const newNode = new TreeNode(nums[mid]);
-        if (parent == null) {
-            // Set the root
-            root = newNode;
-        } else if (childType == "left") {
-            parent.left = newNode;
-        } else { // right
-            parent.right = newNode;
-        }
-
-        // Compute range of left + push
-        const leftRange = (mid - 1) - left + 1;
-        //console.log(leftRange, "left range");
-        if (leftRange >= 1) stack.push([left, mid - 1, newNode, "left"]);
-
-        // Compute range of right + push
-        const rightRange = right - (mid + 1) + 1
-        //console.log(rightRange, "right range");
-        if (rightRange >= 1) stack.push([mid + 1, right, newNode, "right"]);
-
-        //console.log(stack);
-    }
+    const root = addMid(0, nums.length - 1);
 
     return root;
 };
