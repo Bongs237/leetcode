7040907e28f3094aa0e6bd463a7f74ac139dc1b5
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-09 12:14:20 -0700

    Time: 3 ms (66.40%) | Memory: 19.1 MB (90.28%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -1,3 +1,6 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
+        # silly trick:
+        # if a string is a repeated substring then
+        # if you double the string and yoink the first and last letters, you should be able to find the original string in it
         return s in (s * 2)[1:-1]
