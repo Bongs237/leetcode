75af0f0ed84d38a368e61e010e8637c5b244ab6f
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-09 16:01:39 -0700

    Time: 1 ms (97.05%) | Memory: 44.6 MB (20.80%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        
+        LinkedList<TreeNode> q = new LinkedList<>();
+
+        // to start off, you add the only node on the top level, the root
+        q.addLast(root);
+        while (!q.isEmpty()) {
+            // EVERY TIME we get to this point of the loop,
+            // all nodes of the current level we're traversing should be in the queue
+
+            // therefore the size of the level = size of the queue
+            // that's the trick
+            int levelSize = q.size();
+
+            List<Integer> currentList = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                // enqueue both of the children from each node on this level
+                TreeNode item = q.peek();
+                if (item.left != null) q.addLast(item.left);
+                if (item.right != null) q.addLast(item.right);
+
+                // add each node on this level to the list
+                currentList.add(item.val);
+                // kill each node on the current level
+                q.removeFirst();
+            }
+            ans.add(currentList);
+        }
+
+        List<List<Integer>> ans2 = new ArrayList<>();
+        for (int i = ans.size() - 1; i >= 0; i--) {
+            ans2.add(ans.get(i));
+        }
+        return ans2;
+    }
+}
