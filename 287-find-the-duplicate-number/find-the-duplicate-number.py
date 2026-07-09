e0c2efa1f9f7aff9526e68211ed280471bafab9c
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-09 10:25:02 -0700

    Time: 80 ms (13.84%) | Memory: 33.4 MB (73.43%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,23 +1,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         # "meta list problem" lol
+        # Turn into linked list where index i points to nums[i]
+        # then to do node.next.next you do nums[nums[i]]
 
         # 0 -> 3 -> 4 -> 2 -> 3....
 
+        # Find meeting point
         slow = nums[0]
         fast = nums[slow]
         while slow != fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
 
-        print("stop at", slow, fast)
-
+        # Set it to 0. Somehow being 1 off just works
+        # And then have fast loop thru the cycle until slow meets fast and thats where the loop begins
         slow = 0
         while slow != fast:
             print(slow, fast)
             slow = nums[slow]
             fast = nums[fast]
-
-        print(slow, fast)
-
         return slow
