8c81e1898cd20d46ddc09420745745bd81af51eb
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-09 13:39:17 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (47.85%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.py
@@ -0,0 +1,3 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2 # this is the most profoundly complex statement of all time. computer scientists still wonder what this line of code means
