31c87234c5d258421f409855d7a8dfa7563f9a9e
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-09 13:30:21 -0700

    Time: 281 ms (5.36%) | Memory: 32 MB (6.17%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if len(nums) <= 1:
+            return True
+
+        last_change = 0
+        for i in range(len(nums) - 1):
+            j = i + 1
+            print(nums[i], nums[j], last_change)
+            new_change = nums[j] - nums[i]
+            if new_change == 0:
+                continue
+
+            if last_change == 0:
+                last_change = new_change
+                continue
+
+            if new_change < 0 and not last_change < 0:
+                print("case 1")
+                return False
+
+            if new_change > 0 and not last_change > 0:
+                print("case 2")
+                return False
+
+            last_change = new_change
+
+        return True
