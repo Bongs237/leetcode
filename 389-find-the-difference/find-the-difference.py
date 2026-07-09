e370f2f9ca7ce64f04228d0ed5a814ebb015c05f
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-09 11:58:21 -0700

    Time: 4 ms (22.15%) | Memory: 19.1 MB (90.16%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,5 +1,7 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        # Because xor the same character twice e.g. a ^ a = 0
+        # So then if there's one char that is different, that will be the result of xor'ing every character of s and t
         c = 0
 
         for ch in s:
