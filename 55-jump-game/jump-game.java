1b3ff07777847edbc07cd53ba5e9555efb522b17
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-10 11:10:08 -0700

    Time: 3 ms (22.45%) | Memory: 47.8 MB (80.95%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        /*
+        2,3,1,1,4
+        2 4
+
+        3,2,1,0,4
+        3 3 3 3
+        Is current index <= a position we already found?
+        if no, skip
+
+        3,2,1,0,1,2
+        3 3 3 3 
+
+        */
+        if (nums.length == 1) return true;
+        if (nums.length == 2) return nums[0] != 0;
+
+        int maxFound = -1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i == 0 || i <= maxFound) {
+                int jumpPos = i + nums[i];
+                // Cuz that's the maximum jump pos,
+                // so if you can jump way more than the last element, then you can def jump to the last element
+                if (jumpPos >= nums.length - 1) {
+                    return true;
+                }
+                maxFound = Math.max(maxFound, jumpPos);
+            }
+        }
+        return false;
+    }
+}
