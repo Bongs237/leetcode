8e5368a973bc00196f16c0ba1b9b648478bb5fac
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-11 15:33:19 -0700

    Time: 259 ms (5.12%) | Memory: 22.6 MB (6.06%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -12,38 +12,54 @@ class Solution:
         Find connected components
         Eliminate components that are not complete
         """
+
+        # Jank class
+        class Pair:
+            def __init__(self, m, n):
+                self.m = m
+                self.n = n
+
+            def __str__(self):
+                return "Pair[" + str(self.m) + ", " + str(self.n) + "]"
+
+            def __repr__(self):
+                return str(self)
+
+            def __eq__(self, other):
+                return (self.m == other.m and self.n == other.n) or (self.m == other.n and self.n == other.m)
+
+            def __hash__(self):
+                return hash(str(min(self.m, self.n)) + " " + str(max(self.m, self.n)))
+
         # global
         visited = set()
         comp = []
 
         def bfs(src):
-            #distance = [-1] * n
             visited = set()
             queue = deque()
 
             queue.append(src)
             visited.add(src)
-            #distance[src] = 0
 
             while queue:
                 curr = queue.popleft()
                 for child in adj[curr]:
                     if child not in visited:
                         visited.add(child)
-                        #distance[child] = distance[curr] + 1
-                        #print(distance)
                         queue.append(child)
 
             return visited
 
         adj = [[] for i in range(n)]
+        edge_pairs = set()
         for edge in edges:
             adj[edge[0]].append(edge[1])
             adj[edge[1]].append(edge[0])
+            edge_pairs.add(Pair(edge[0], edge[1]))
 
         for i in range(n):
             if i not in visited:
-                print("callign bfs on",i)
                 vis_for_node = bfs(i)
                 visited.update(vis_for_node)
                 comp.append(vis_for_node)
@@ -52,12 +68,10 @@ class Solution:
         for nodes in comp:
             nodes = list(nodes)
             is_complete = True
-            # o n cubed oh heck nah
             for i in range(len(nodes)):
                 for j in range(i+1, len(nodes)):
-                    first, second = nodes[i], nodes[j]
-                    #print("testing", first, second)
-                    if not([first, second] in edges or [second, first] in edges):
+                    p = Pair(nodes[i], nodes[j])
+                    if p not in edge_pairs:
                         is_complete = False
                         break
 
