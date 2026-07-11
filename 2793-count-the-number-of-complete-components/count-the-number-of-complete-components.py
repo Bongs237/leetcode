389a234d14c4bf3c3bf9f77295585b54086bb507
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-11 16:07:40 -0700

    Time: 36 ms (72.05%) | Memory: 19.8 MB (72.20%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -1,17 +1,5 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        """
-        adj:
-        0: 1, 2
-        1: 0, 2
-        2: 0, 1
-        3: 4
-        4: 3
-        5: -
-
-        Find connected components
-        Eliminate components that are not complete
-        """
         # global
         visited = set()
         comp = []
