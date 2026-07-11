66a46622a33004c471a920edbb720fb7d6dda161
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-11 15:23:26 -0700

    Time: 652 ms (5.12%) | Memory: 20 MB (48.60%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -0,0 +1,66 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        """
+        adj:
+        0: 1, 2
+        1: 0, 2
+        2: 0, 1
+        3: 4
+        4: 3
+        5: -
+
+        Find connected components
+        Eliminate components that are not complete
+        """
+        # global
+        visited = set()
+        comp = []
+
+        def bfs(src):
+            #distance = [-1] * n
+            visited = set()
+            queue = deque()
+
+            queue.append(src)
+            visited.add(src)
+            #distance[src] = 0
+
+            while queue:
+                curr = queue.popleft()
+                for child in adj[curr]:
+                    if child not in visited:
+                        visited.add(child)
+                        #distance[child] = distance[curr] + 1
+                        #print(distance)
+                        queue.append(child)
+
+            return visited
+
+        adj = [[] for i in range(n)]
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+
+        for i in range(n):
+            if i not in visited:
+                print("callign bfs on",i)
+                vis_for_node = bfs(i)
+                visited.update(vis_for_node)
+                comp.append(vis_for_node)
+
+        ans = 0
+        for nodes in comp:
+            nodes = list(nodes)
+            is_complete = True
+            for i in range(len(nodes)):
+                for j in range(i+1, len(nodes)):
+                    first, second = nodes[i], nodes[j]
+                    print("testing", first, second)
+                    if not([first, second] in edges or [second, first] in edges):
+                        is_complete = False
+                        break
+
+            if is_complete:
+                ans += 1
+
+        return ans
