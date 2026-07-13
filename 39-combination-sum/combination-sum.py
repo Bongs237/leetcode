018ce323510151b3c6f077386fadac235dc7b4e2
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-13 13:10:18 -0700

    Time: 19 ms (12.13%) | Memory: 19.8 MB (8.52%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,26 +1,3 @@
-class Combo:
-    def __init__(self, items: List[int]):
-        self.items = items
-
-    def __hash__(self):
-        return hash(str(self.items))
-
-    def __eq__(self, other):
-        return self.items == other.items
-
-    def with_new_item(self, item, freq=1):
-        items_copy = list(self.items)
-        for i in range(freq):
-            items_copy.append(item)
-
-        return Combo(items_copy)
-
-    def __str__(self):
-        return str(self.items)
-
-    def __repr__(self):
-        return str(self)
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         combos = []
@@ -29,7 +6,7 @@ class Solution:
             #print("i", i, "target", target_left, "combo", combo)
 
             if target_left == 0:
-                combos.append(combo.items)
+                combos.append(combo)
                 return
             elif target_left < 0:
                 return
@@ -42,8 +19,8 @@ class Solution:
             for poss_freq in range(0, max_freq + 1):
                 next_targ_left = target_left - (poss_freq * candidates[i])
                 #print("calling recursively", next_targ_left)
-                form_sum(i + 1, next_targ_left, combo.with_new_item(candidates[i], poss_freq))
+                form_sum(i + 1, next_targ_left, combo + [candidates[i]] * poss_freq)
 
-        form_sum(0, target, Combo([]))
+        form_sum(0, target, [])
 
         return combos
