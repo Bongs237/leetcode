df09896bfbfacd3fa3c532074b1c621bf30cb107
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-13 13:10:49 -0700

    Time: 19 ms (12.13%) | Memory: 19.5 MB (82.09%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # It's all in the recursion
         combos = []
 
         def form_sum(i, target_left, combo):
-            #print("i", i, "target", target_left, "combo", combo)
-
             if target_left == 0:
                 combos.append(combo)
                 return
@@ -14,11 +13,9 @@ class Solution:
                 return
 
             max_freq = target_left // candidates[i]
-            #print("max freq", max_freq)
 
             for poss_freq in range(0, max_freq + 1):
                 next_targ_left = target_left - (poss_freq * candidates[i])
-                #print("calling recursively", next_targ_left)
                 form_sum(i + 1, next_targ_left, combo + [candidates[i]] * poss_freq)
 
         form_sum(0, target, [])
