7d300bfaadb4fd98d434ace1b34991960e66e24f
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-15 12:23:02 -0700

    Time: 67 ms (20.17%) | Memory: 28.6 MB (75.14%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -15,16 +15,8 @@ class Solution:
         # 6: 4
         # 4: -1
 
-        # but max is O(n)
-        # go backwards
-
-        max_val = 0
-        max_found = 0
-        for i in range(len(prices) - 1, -1, -1):
-            max_val = max(prices[i], max_val)
-
-            if i != len(prices) - 1:
-                max_found = max(max_found, max_val - prices[i])
+        # but max function is O(n)
+        # go backwards and track the max instead. big brain
 
         # then you find the differences
         # 7: 6-7=-1
@@ -32,5 +24,11 @@ class Solution:
         # 5: 6-5=1
         # ...
         # then you get max of that
+
+        max_val = 0
+        max_found = 0
+        for i in range(len(prices) - 1, -1, -1):
+            max_val = max(prices[i], max_val)
+            max_found = max(max_found, max_val - prices[i])
             
         return 0 if max_found < 0 else max_found
