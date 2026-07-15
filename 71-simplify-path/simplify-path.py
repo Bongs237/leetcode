89d43f19b06849ae28e8f08de3418e2306f3e501
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-15 08:42:10 -0700

    Time: 3 ms (38.40%) | Memory: 19.2 MB (61.66%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,21 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        # split path
+        path_list = path.split("/")
+        print(path_list)
+        new_path = []
+
+        for i, folder in enumerate(path_list):
+            # remove multiple slashes (empty item)
+            # remove . directories
+            if folder == '' or folder == '.':
+                continue
+            
+            # remove directories preceding ..
+            if folder == '..':
+                if new_path:
+                    new_path.pop()
+            else:
+                new_path.append(folder)
+
+        return "/" + "/".join(new_path)
