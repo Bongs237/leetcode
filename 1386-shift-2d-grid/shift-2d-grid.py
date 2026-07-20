aca12a03edc60922c59ed82530fbc455852c39d7
Ben <10395903+Bongs237@users.noreply.github.com>
2026-07-19 20:08:24 -0700

    Time: 103 ms (19.96%) | Memory: 19.5 MB (81.69%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.py
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -2,8 +2,6 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         n = len(grid)
         m = len(grid[0])
-
-        print(n, m)
         
         for _ in range(k):
             next_item = grid[-1][-1]
@@ -15,11 +13,7 @@ class Solution:
 
                     next_item = grid[i][j]
 
-                    #print("on", i, j)
-                    #print("Next item", next_item)
-
                     # plant the item
                     grid[i][j] = curr_item
-            print(grid)
 
         return grid
