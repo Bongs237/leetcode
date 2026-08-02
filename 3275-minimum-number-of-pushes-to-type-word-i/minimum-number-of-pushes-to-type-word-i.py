e43a140f4a826c4189e605aa5babae7f266dbad5
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-02 12:07:35 -0400

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.64%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        # If theres 8 distinct letters in word, you can remap so every keypad has one letter (8 presses)
+        # If 9, 8 presses + two presses on one 8+2*1 = 10
+        # if 10, 8+2*2
+        
+        # if 17-24, third press
+        # if 25, 26 - fourth press on 7 and 9
+
+        """
+        a
+        c
+        o
+        l
+
+        """
+        print(len(word))
+
+        max_press = math.ceil(len(word) / 8)
+
+        ans = 0
+
+        if max_press >= 1:
+            ans += min(len(word), 8)
+        if max_press >= 2:
+            ans += min((len(word) - 8) * 2, 8 * 2)
+        if max_press >= 3:
+            ans += min((len(word) - 8 * 2) * 3, 8 * 3)
+        if max_press >= 4:
+            if len(word) == 25:
+                ans += 4
+            else: # 26
+                ans += 8
+
+        return ans
