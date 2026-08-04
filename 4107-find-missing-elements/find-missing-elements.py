731848105f148c1f1cbc00069294e2417edea335
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-04 11:25:09 -0400

    Time: 3 ms (69.59%) | Memory: 19.5 MB (18.85%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -3,14 +3,15 @@ class Solution:
         low = 101
         high = 0
 
-        s = set(nums)
+        s = set()
 
+        # one pass!!!
         for num in nums:
             low = min(low, num)
             high = max(high, num)
+            s.add(num)
 
         ans = []
-
         for i in range(low, high + 1):
             if i not in s:
                 ans.append(i)
