549ddb5cc3f4899cf960a5b853cfd0790fa3fdfa
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-05 15:53:56 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.64%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,6 +1,8 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         """
+Solution sauce: https://leetcode.com/problems/stone-game/solutions/8435492/solution-by-la_castille-s2sg
+
 This one is so weird. Just trust the process
 We can take the piles array, and divide it into all the numbers on even indices and all the numbers on odd indices.
 Let's color the even indices pink and odd indices blue (0-indexed)
