126e5a8af405ee58f30002b3e5ac36b81b7b3ef0
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-06 18:47:49 -0400

    Time: 11 ms (77.29%) | Memory: 19.2 MB (94.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
         freq_s = defaultdict(int)
         freq_t = defaultdict(int)
 
