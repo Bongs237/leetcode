bc25472e8d23870353027f690c0d11a0b01a05f5
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-06 18:49:57 -0400

    Time: 11 ms (77.29%) | Memory: 19.5 MB (30.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -9,14 +9,6 @@ class Solution:
         for ch in s:
             freq_s[ch] += 1
         for ch in t:
-            if ch not in freq_s:
-                return False
-
             freq_t[ch] += 1
 
-        # s and t should have same characters
-        for key in freq_s.keys():
-            if freq_s[key] != freq_t[key]:
-                return False
-
-        return True
+        return freq_s == freq_t
