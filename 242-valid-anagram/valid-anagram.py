fff09bc30621e24d7b6b25c228cb5b2a8dc721e5
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-06 18:46:53 -0400

    Time: 11 ms (77.29%) | Memory: 19.3 MB (75.75%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq_s = defaultdict(int)
+        freq_t = defaultdict(int)
+
+        for ch in s:
+            freq_s[ch] += 1
+        for ch in t:
+            if ch not in freq_s:
+                return False
+
+            freq_t[ch] += 1
+
+        # s and t should have same characters
+        for key in freq_s.keys():
+            if freq_s[key] != freq_t[key]:
+                return False
+
+        return True
