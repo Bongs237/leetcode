bb6995dbdb438e2dc620496215a9ade654083dbc
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-09 10:27:24 -0700

    Time: 55 ms (5.73%) | Memory: 26.9 MB (32.85%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,44 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        everything = 1
-        zeroes = 0
-        for num in nums:
-            if num != 0:
-                everything *= num
-            else:
-                zeroes += 1
-        
-        res = []
-        for num in nums:
-            if num == 0:
-                if zeroes == 1:
-                    res.append(everything)
-                else:
-                    res.append(0)
-            else:
-                if zeroes > 0:
-                    res.append(0)
-                else:
-                    res.append(everything // num)
+        """
+prefix
+1
+1*2
+1*2*4
+1*2*4*6
 
-        return res
+suffix
+6
+6*4
+6*4*2
+6*4*2*1
+
+0. 1 -> 2*4*6 1         * suffix[2]
+1. 2 -> 1*4*6 prefix[0] * suffix[1]
+2. 4 -> 1*2*6 prefix[1] * suffix[0]
+3. 6 -> 1*2*4 prefix[2] * 1
+        """
+
+        prefix = [0] * len(nums)
+        suffix = [0] * len(nums)
+
+        prefix[0] = nums[0]
+        suffix[0] = nums[-1]
+
+        for i in range(1, len(nums)):
+            prefix[i] = nums[i] * prefix[i - 1]
+            suffix[i] = nums[len(nums) - i - 1] * suffix[i - 1]
+
+        ans = []
+        for i in range(0, len(nums)):
+            pref_index = i - 1
+            suff_index = len(nums) - i - 2
+
+            pref_val = prefix[pref_index] if pref_index >= 0 else 1
+            suff_val = suffix[suff_index] if suff_index >= 0 else 1
+
+            ans.append(pref_val * suff_val)
+
+        return ans
+
+        
