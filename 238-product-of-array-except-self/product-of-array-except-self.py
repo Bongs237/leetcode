6f7e0995b1c45c550de1ec1193875fcc2004fc55
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-09 10:44:04 -0700

    Time: 27 ms (42.22%) | Memory: 25.7 MB (43.95%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,44 +1,35 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         """
-prefix
-1
-1*2
-1*2*4
-1*2*4*6
+prefix - product of every element before i, not including i
+If there's no elements before it let's keep it at 1
+0. 1
+1. 1
+2. 1*2
+3. 1*2*4
 
-suffix
-6
-6*4
-6*4*2
-6*4*2*1
+suffix - product of every element after i, not including i
+If there's no elements after it let's keep it at 1
+0. 2*4*6
+1. 4*6
+2. 6
+3. 1
 
-0. 1 -> 2*4*6 1         * suffix[2]
-1. 2 -> 1*4*6 prefix[0] * suffix[1]
-2. 4 -> 1*2*6 prefix[1] * suffix[0]
-3. 6 -> 1*2*4 prefix[2] * 1
+0. 1 -> 2*4*6 prefix[0] * suffix[0]
+1. 2 -> 1*4*6 prefix[1] * suffix[1]
+2. 4 -> 1*2*6 prefix[2] * suffix[2]
+3. 6 -> 1*2*4 prefix[3] * suffix[3]
+
         """
 
-        prefix = [0] * len(nums)
-        suffix = [0] * len(nums)
-
-        prefix[0] = nums[0]
-        suffix[0] = nums[-1]
+        ans = [1] * len(nums)
 
         for i in range(1, len(nums)):
-            prefix[i] = nums[i] * prefix[i - 1]
-            suffix[i] = nums[len(nums) - i - 1] * suffix[i - 1]
+            ans[i] = ans[i - 1] * nums[i - 1]
 
-        ans = []
-        for i in range(0, len(nums)):
-            pref_index = i - 1
-            suff_index = len(nums) - i - 2
-
-            pref_val = prefix[pref_index] if pref_index >= 0 else 1
-            suff_val = suffix[suff_index] if suff_index >= 0 else 1
-
-            ans.append(pref_val * suff_val)
+        suffix = 1
+        for i in range(len(nums) - 2, -1, -1):
+            suffix = suffix * nums[i + 1]
+            ans[i] *= suffix
 
         return ans
-
-        
