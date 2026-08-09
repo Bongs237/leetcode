0acc66a4c80b2d6de8f31f13cb0b68ec85f589df
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-09 14:58:38 -0700

    Time: 6 ms (85.13%) | Memory: 20.7 MB (19.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,22 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        isalnum = lambda character: character.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"
-        
-        i = 0
-        j = len(s) - 1
-
-        while i < j:
-            if not isalnum(s[i]):
-                i += 1
-                continue
-            if not isalnum(s[j]):
-                j -= 1
-                continue
-
-            if s[i].lower() != s[j].lower():
-                return False
-
-            i += 1
-            j -= 1
-
-        return True
+        s = re.sub(r"[^a-z0-9]", "", s.lower())
+        return s == s[::-1]
