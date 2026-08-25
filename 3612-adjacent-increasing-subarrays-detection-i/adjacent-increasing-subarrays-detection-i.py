2d29704f2641b66ae32d3c8f01e38d94d6b09890
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-24 23:56:53 -0400

    Time: 92 ms (33.09%) | Memory: 19.4 MB (18.71%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
@@ -1,25 +1,15 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        subarr_len = [1]
-        
-        for i in range(1, len(nums)):
-            if nums[i - 1] < nums[i]:
-                subarr_len[-1] += 1
+        streak = 0
+
+        for j in range(k, len(nums)):
+            i = j - k
+            print(i, j)
+            if i == 0 or (nums[i - 1] < nums[i] and nums[j - 1] < nums[j]):
+                streak += 1
+                if streak == k:
+                    return True
             else:
-                subarr_len.append(1)
-        
-        for i in range(len(subarr_len)):
-            last = 0
-            if i > 0:
-                last = subarr_len[i - 1]
-
-            curr = subarr_len[i]
-
-            # there are at least k elements in the current increasing subarray AND at least k elements in the last increasing subarray
-            # OR, the two subarrays in the solution are inside one of our subarrays, so the subarray would have to be at least 2k long
-            # -> e.g. [-10, 5] k=1; subarr_len would equal [2]...
-            # but we can make two subarrays [-10] and [5]
-            if curr >= k and last >= k or (curr >= 2 * k):
-                return True
+                streak = 1
                 
         return False
