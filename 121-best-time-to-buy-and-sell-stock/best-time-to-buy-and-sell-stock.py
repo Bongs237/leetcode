ae89acf5042a4ce37db422b4a2683df695a64753
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-30 19:12:32 -0400

    Time: 61 ms (28.72%) | Memory: 28.7 MB (20.94%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,29 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # for every day, theres an optimal day to sell the stock to max the profits
-        # thats just the maximum value after the day
-        # 7, 1, 5, 3, 6, 4
-        # 7: 6
-        # 1: 6
-        # 5: 6
-        # 3: 6
-        # 6: 4
-        # 4: -1
+        max_profit = 0
+        overall_min = prices[0]
 
-        # but max function is O(n)
-        # go backwards and track the max instead. big brain
+        for i in range(1, len(prices)):
+            profit_if_sold = prices[i] - overall_min
+            max_profit = max(max_profit, profit_if_sold)
 
-        # then you find the differences
-        # 7: 6-7=-1
-        # 1: 6-1=5
-        # 5: 6-5=1
-        # ...
-        # then you get max of that
+            overall_min = min(overall_min, prices[i])
 
-        max_val = 0
-        max_found = 0
-        for i in range(len(prices) - 1, -1, -1):
-            max_val = max(prices[i], max_val)
-            max_found = max(max_found, max_val - prices[i])
-            
-        return 0 if max_found < 0 else max_found
+        return max_profit
