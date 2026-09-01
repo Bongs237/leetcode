521943101f3bbb13393499206243bd8a1175afec
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-31 22:47:45 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.81%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,9 +1,7 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        # due to integer limit, you'll only need to sum the digits thrice
-        num = num // (10**9) + (num // (10**8)) % 10 + (num // (10**7)) % 10 + (num // (10**6)) % 10 + (num // (10**5)) % 10 + (num // (10**4)) % 10 + (num // (10**3)) % 10 + (num // (10**2)) % 10 + (num // (10**1)) % 10 + num % 10
-
-        num = (num // 10) + (num % 10)
-        num = (num // 10) + (num % 10)
-
-        return num
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
