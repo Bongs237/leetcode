9abceedbbb7e3f8a94112b419deb07142a05f6f9
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-31 22:33:07 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.81%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        """
+        you know what
+        maybe we wake up early tmr
+        and actually be productive
+        I WILL find the O(1) solution manifesting rn
+        """
+
+        num = num // (10**9) + (num // (10**8)) % 10 + (num // (10**7)) % 10 + (num // (10**6)) % 10 + (num // (10**5)) % 10 + (num // (10**4)) % 10 + (num // (10**3)) % 10 + (num // (10**2)) % 10 + (num // (10**1)) % 10 + num % 10
+
+        num = (num // 10) + (num % 10)
+        num = (num // 10) + (num % 10)
+
+        return num
