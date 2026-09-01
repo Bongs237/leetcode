5dd7e16f6b513e4c278a2489b1ab1629b379ab4b
Ben <10395903+Bongs237@users.noreply.github.com>
2026-08-31 22:34:54 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.33%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -6,7 +6,7 @@ class Solution:
         and actually be productive
         I WILL find the O(1) solution manifesting rn
         """
-
+        # due to integer limit, you'll only need to sum the digits thrice
         num = num // (10**9) + (num // (10**8)) % 10 + (num // (10**7)) % 10 + (num // (10**6)) % 10 + (num // (10**5)) % 10 + (num // (10**4)) % 10 + (num // (10**3)) % 10 + (num // (10**2)) % 10 + (num // (10**1)) % 10 + num % 10
 
         num = (num // 10) + (num % 10)
