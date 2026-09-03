7f13cbff7669ac2852b10cf08565f145f73fcefe
Ben <10395903+Bongs237@users.noreply.github.com>
2026-09-02 20:04:00 -0400

    Time: 3 ms (12.53%) | Memory: 19.1 MB (97.74%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -1,13 +1,13 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        # 5-3=2 5-2=3 5-4=1 odd-odd=even(cancels) odd-even=odd
-        # even-even=even 34-20=14 28-16=12
-        # even-odd=odd 4-5=-1
-
         # odd-odd=even
         # odd-even=odd
         # even-even=even
         # even-odd=odd
 
-        # 3 5 9 2 4 6
+        # if it's all odd, you can make all of em odd
+        # if it's all even, you can make all of em even
+        # if it's a mix, you can make it whatever the majority parity is
+        # if # of odd = # of even, you can make it either
+        
         return True
