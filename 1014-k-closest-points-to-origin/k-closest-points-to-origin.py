22d21d352b29db229f3746e74f59cea932190738
Ben <10395903+Bongs237@users.noreply.github.com>
2026-09-03 21:15:25 -0400

    Time: 39 ms (94.08%) | Memory: 25.3 MB (23.30%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,3 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return [[x, y] for dist, x, y in (sorted([(math.sqrt(x ** 2 + y ** 2), x, y) for x, y in points])[:k])]
+        return [[x, y] for dist, x, y in (sorted([(x ** 2 + y ** 2, x, y) for x, y in points])[:k])]
